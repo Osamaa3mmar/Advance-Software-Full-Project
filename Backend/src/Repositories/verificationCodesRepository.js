21bbc9369e408957aafb3dafdfbae59d46b1a611
Osamaa3mmar <osamaammarothman@gmail.com>
2025-10-12 18:13:19 +0300

    Fix: make stander urls

--- Backend/src/Repositories/verificationCodesRepository.js
+++ Backend/src/Repositories/verificationCodesRepository.js
@@ -1,10 +1,11 @@
-import { connection } from "../../Database/Connection.js"
+import { connection } from "../../Database/Connection.js";
 
 export class VerificationCodesRepository {
-    static createVerificationCode=async(userId,code,type)=>{
-        const [rows]=await connection.query("INSERT INTO verification_codes (target_id,code,target_type,expires_at) VALUES (?,?,?,DATE_ADD(NOW(), INTERVAL 1 DAY))",[userId,code,type]);
-        return rows;
-    }
-
-
+  static createVerificationCode = async (userId, code, type) => {
+    const [result] = await connection.query(
+      "INSERT INTO verification_codes (target_id,code,target_type,expires_at) VALUES (?,?,?,DATE_ADD(NOW(), INTERVAL 1 DAY))",
+      [userId, code, type]
+    );
+    return result.insertId;
+  };
 }
--- Backend/src/initControllers.js
+++ Backend/src/initControllers.js
@@ -1,15 +1,6 @@
 import authRouter from "./Routes/AuthRouter.js";
-import userRouter from "./Routes/UsersRouter.js"
 
 export const initControllers=(app)=>{
-
-app.use("/users",userRouter);
-
-
-
-
-
-
 
 
 app.use("/api/auth",authRouter);
