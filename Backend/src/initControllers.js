97d1f73d1bc95b323cc5c103157cf03844d21804
Mohamad-Salha <mohamadsalha88@gmail.com>
2025-10-19 18:15:27 +0200

    Capital w

--- Backend/src/initControllers.js
+++ Backend/src/initControllers.js
@@ -1,6 +1,6 @@
 import userRouter from "./Routes/UsersRouter.js";
 import healthGuidesRoutes from "./Routes/healthGuidesRoutes.js";
-import workshopsRouter from "./Routes/WorkshopsRouter.js";
+import workshopsRouter from "./Routes/workshopsRouter.js";
 import authRouter from "./Routes/AuthRouter.js";
 export const initControllers = (app) => {
 	app.use("/healthGuides", healthGuidesRoutes);
